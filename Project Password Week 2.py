4dd70d7b391285f3f9ca47ab8a92f7831ec70356
Furious Panda <107466562+FuriousPanda7@users.noreply.github.com>
2022-06-20 11:30:53 +0430

    Project Password Week 2

---
+++ Project Password Week 2.py
@@ -0,0 +1,22 @@
+
+password = input("Plz Type Your Password: \n")
+#Must Contain At Least 8 Words
+if len(password) >= 8 :
+# Must Not Be Empty
+    if password.isspace() != True:
+#Must Contain Both Word And Numbers And Not Space Between Them
+        if password.isalnum()  :
+#Must Not Start With Number
+            if password[0].isnumeric() != True:
+            
+                print("..:##:..".join(password),)
+            else:
+                print("Your Password Must Not Start With A Number ")
+
+        else:
+             print("Your Password Must Contain Numbers And Words And Must Have Space At the Begginig Or Between Characters ")
+    else:
+        print("Your Password Should Not Be Empty")
+else:
+    print("Your Password Must Have At Least 8 Charachter ")
+            
